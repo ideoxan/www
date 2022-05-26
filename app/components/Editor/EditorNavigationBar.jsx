143b88a803ff57638b81ab7715fb8d004d9939f3
Skyclo <hello.skyclo@gmail.com>
2022-05-26 14:07:57 -0400

    ✨ add editor nav bar

---
+++ app/components/Editor/EditorNavigationBar.jsx
@@ -0,0 +1,77 @@
+import Icon from 'app/components/Icon'
+
+export default function EditorNavigationBar({
+    courseUUID,
+    lessonIndex,
+    lessonName,
+    chapterIndex,
+    courseName,
+    navigationNext,
+    navigationPrevious
+}) {
+
+    let navIcons = {
+        size: 4,
+        stroke: 2,
+        color: 'gray-50'
+    }
+
+    return (
+        <nav className="w-full bg-gray-700 flex flex-row py-3 px-6 border-b border-b-gray-500 border-opacity-20">
+
+            {/* Ideoxan Logo */}
+            <div className="flex my-auto w-1/4">
+                <img src="/images/ix_logo_white_trans_253x50.png" alt="Ideoxan" className="h-3 opacity-50" />
+            </div>
+
+            {/* Lesson Header */}
+            <h1 className="font-sans font-bold text-xs text-center w-1/2 text-gray-50 my-auto">Lesson {lessonIndex + 1}: {lessonName} (Chapter {chapterIndex + 1}) &mdash; {courseName}</h1>
+
+            {/* Navigation Buttons */}
+            <div className="flex flex-row w-1/4 space-x-4 justify-end my-auto">
+
+                {/* Previous Lesson */}
+                {navigationPrevious && (
+                    <a href={"/learn/" + courseUUID + "/" + navigationPrevious[0] + "/" + navigationPrevious[1]}>
+                        <Icon
+                            name="ArrowLeft"
+                            width={navIcons.size}
+                            height={navIcons.size}
+                            color={navIcons.color}
+                            strokeThickness={navIcons.stroke}
+                            className="my-auto opacity-50 hover:opacity-100"
+                        />
+                    </a>
+                )}
+
+                {/* Courses Page */}
+                <a href={"/courses/#" + courseUUID}>
+                    <Icon
+                        name="Library"
+                        width={navIcons.size}
+                        height={navIcons.size}
+                        color={navIcons.color}
+                        strokeThickness={navIcons.stroke}
+                        className="my-auto opacity-50 hover:opacity-100"
+                    />
+                </a>
+
+                {/* Next Lesson */}
+                {navigationNext && (
+                    <a href={"/learn/" + courseUUID + "/" + navigationNext[0] + "/" + navigationNext[1]}>
+                        <Icon
+                            name="ArrowRight"
+                            width={navIcons.size}
+                            height={navIcons.size}
+                            color={navIcons.color}
+                            strokeThickness={navIcons.stroke}
+                            className="my-auto opacity-50 hover:opacity-100"
+                        />
+                    </a>
+                )}
+
+            </div>
+
+        </nav>
+    )
+}
--- app/routes/learn/$courseId/$chapterId/$lessonId.jsx
+++ app/routes/learn/$courseId/$chapterId/$lessonId.jsx
@@ -1,72 +1,83 @@
 import { json } from "@remix-run/node"
 import { useLoaderData } from "@remix-run/react"
+import EditorNavigationBar from "app/components/Editor/EditorNavigationBar"
 
 export const loader = async ({ params }) => {
     // TODO: use params.courseId, params.chapterId, params.lessonId to load the lesson from
     // TODO: supabase and serve it. (GraphQL?)
-    return json(
-        {
-            course: {
-                name: "Intro to NodeJS (Testing Sample)",
-                uuid: "07e68b9a-d10b-452f-acf7-53a200caecb3",
-                description: "A sample course for testing the Ideoxan website using NodeJS",
-                tags: ["nodejs", "testing", "javascript"],
-                author: ["@ideoxan"],
-            },
-            chapter: {
-                name: "Introduction",
-                index: 0,
-            },
-            lesson: {
-                name: "What is NodeJS?",
-                index: 0,
-                quiz: false,
-                content: {
-                    guide: "Hello, World!",
-                    workspace: {
-                        "index.js": "function sum(a, b) {\n  return a + b\n}\n\nconsole.log(sum(1, 2))",
-                        "index.html": "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<meta charset=\"UTF-8\">\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n<title>Document</title>\n</head>\n<body>\n<h1>Hello, World!</h1>\n<p>Welcome to my website!</p>\n</body>\n</html>",
-                        "src/b.txt": "lol\n"
-                    }
-                },
-                tasks: [
-                    {
-                        instructions: "Run the project",
-                        completedByDefault: false,
-                        conditions: [
-                            {
-                                type: "file",
-                                file: "index.js",
-                                content: "function sum(a, b) {\n  return a + b\n}\n\nconsole.log(sum(1, 2))",
-                                is: "equal",
-                            },
-                            {
-                                type: "run",
-                                successful: true,
-                            }
-                        ]
-                    }
-                ],
-                environment: {
-                    tesseract: true,
-                    on: "node:latest",
-                    commands: [
-                        "npm install",
-                    ],
-                    viewport: false,
-                    console: true
+    return json({
+        course: {
+            name: "Intro to NodeJS (Testing Sample)",
+            uuid: "07e68b9a-d10b-452f-acf7-53a200caecb3",
+            description: "A sample course for testing the Ideoxan website using NodeJS",
+            tags: ["nodejs", "testing", "javascript"],
+            author: ["@ideoxan"],
+        },
+        chapter: {
+            name: "Introduction",
+            index: 0,
+        },
+        lesson: {
+            name: "What is NodeJS?",
+            index: 0,
+            quiz: false,
+            content: {
+                guide: "Hello, World!",
+                workspace: {
+                    "index.js": "function sum(a, b) {\n  return a + b\n}\n\nconsole.log(sum(1, 2))",
+                    "index.html": "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<meta charset=\"UTF-8\">\n<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n<title>Document</title>\n</head>\n<body>\n<h1>Hello, World!</h1>\n<p>Welcome to my website!</p>\n</body>\n</html>",
+                    "src/b.txt": "lol\n"
                 }
+            },
+            tasks: [
+                {
+                    instructions: "Run the project",
+                    completedByDefault: false,
+                    conditions: [
+                        {
+                            type: "file",
+                            file: "index.js",
+                            content: "function sum(a, b) {\n  return a + b\n}\n\nconsole.log(sum(1, 2))",
+                            is: "equal",
+                        },
+                        {
+                            type: "run",
+                            successful: true,
+                        }
+                    ]
+                }
+            ],
+            environment: {
+                tesseract: true,
+                on: "node:latest",
+                commands: [
+                    "npm install",
+                ],
+                viewport: false,
+                console: true
+            },
+            navigation: {
+                next: [0, 1],
+                previous: null,
             }
         }
-    )
+    })
 }
 
 export default function Editor() {
-    const { metadata } = useLoaderData()
+    const metadata = useLoaderData()
 
     return (
         <>
-
+            <EditorNavigationBar
+                courseUUID={metadata.course.uuid}
+                lessonIndex={metadata.lesson.index}
+                lessonName={metadata.lesson.name}
+                chapterIndex={metadata.chapter.index}
+                courseName={metadata.course.name}
+                navigationNext={metadata.lesson.navigation.next}
+                navigationPrevious={metadata.lesson.navigation.previous}
+            />
         </>
     )
 }
