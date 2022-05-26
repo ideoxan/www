1128c97cf1f59e7987c1a35353183f17edb548cc
Skyclo <hello.skyclo@gmail.com>
2022-05-25 21:20:16 -0400

    ✨ add courses preview section

---
+++ app/components/Home/CourseCard.jsx
@@ -0,0 +1,31 @@
+export default function CourseCard({
+    title = null,
+    description = null,
+    comingSoon = false,
+    infoURL = null,
+    startURL = null,
+    className = ""
+}) {
+
+    if (title == null || description == null) {
+        throw new Error("Expected title and description to be defined.")
+    }
+
+    let classNameString = "flex flex-col px-4 py-3 bg-gray-700 rounded-lg ring-1 ring-gray-500 ring-opacity-50 w-full h-full shadow-lg" + className
+
+    return (
+        <div className={classNameString}>
+            <h3 className="font-sans text-gray-50 text-left tracking-tight text-xl font-bold">{title}</h3>
+            {comingSoon ? (
+                <div className="mt-1 bg-primary bg-opacity-50 text-secondary font-sans font-semibold text-2xs rounded-full px-2 py-1 w-max">
+                    Coming Soon
+                </div>
+            ) : null}
+            <p className="paragraph h-full text-sm my-2 text-gray-50 text-left truncate">{description}</p>
+            <div className="flex flex-row w-full">
+                <a href={infoURL} className="inline-block text-gray-50 opacity-50 hover:opacity-100 text-sm font-medium tracking-tight">Learn More</a>
+                <a href={startURL} className="ml-auto inline-block text-secondary opacity-100 hover:opacity-80 text-sm font-extrabold tracking-tight">Start --&gt;</a>
+            </div>
+        </div>
+    )
+}
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -3,6 +3,7 @@ import HeroSection from "app/components/Hero"
 import FeaturesSection from "app/components/Home/FeaturesSection"
 import SolutionsCTA from "app/components/Home/SolutionsCTA"
 import FreeForAllSection from "app/components/Home/FreeForAllSection"
+import CoursesSection from "app/components/Home/CoursesSection"
 
 export default function Index() {
     return (
@@ -12,6 +13,7 @@ export default function Index() {
             <FeaturesSection />
             <SolutionsCTA />
             <FreeForAllSection />
+            <CoursesSection />
         </>
     )
 }
