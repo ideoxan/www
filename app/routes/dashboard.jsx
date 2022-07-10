20b609be94e70067531cb5d27b155816b9951104
Skyclo <hello.skyclo@gmail.com>
2022-07-10 17:47:00 -0400

    💡 add comments

--- app/routes/dashboard.jsx
+++ app/routes/dashboard.jsx
@@ -4,12 +4,16 @@ import { authenticator } from "app/utils/auth.server.js"
 import { supabaseAdmin } from "app/utils/db.server.js"
 
 export async function loader({ request }) {
+    // Check user auth
     let session = await authenticator.isAuthenticated(request)
+    // If the user isn't authenticated, redirect to the login page
     if (!session) throw redirect("/login")
 
+    // If the user session is bad, redirect to the login page
     let { user } = session
     if (!user || !user.id) throw redirect("/login")
 
+    // If the user is authenticated, get the user's data from the database
     let { data: userData, error } = supabaseAdmin.from("user_data")
         .select()
         .eq("id", user.id)
