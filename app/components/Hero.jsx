0d88375766c679228b890abfaa1f609940355a2d
Skyclo <hello.skyclo@gmail.com>
2022-08-01 19:23:23 -0400

    💄 add glow header

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -7,9 +7,9 @@ export default function HeroSection() {
 
                 {/* Header and tagline */}
                 <div className="flex flex-col mt-20 lg:mt-40 mb-4 md:mb-10 lg:mb-16 mx-auto max-w-3xl lg:max-w-7xl lg:w-full p-8">
-                    <h1 className="mx-auto font-sans font-extrabold tracking-tight text-4xl sm:text-5xl md:text-6xl text-center text-gray-50">Learn computer science for free.</h1>
-                    <h2 className="mx-auto mt-6 font-sans font-medium tracking-tight text-sm sm:text-base md:text-lg text-center text-gray-50 opacity-80"> Ideoxan is empowering the next generation of programmers, makers, and inventors.</h2>
-                    <a href="/signup" className="mx-auto mt-12 bttn bttn-outline bttn-normal bttn-outline-white">Join the Waitlist --&gt;</a>
+                    <h1 className="mx-auto font-sans font-extrabold tracking-tight text-4xl sm:text-5xl md:text-6xl text-center text-gray-50 glow-text-white">Learn computer science for free.</h1>
+                    <h2 className="mx-auto mt-6 font-sans font-semibold tracking-tight text-sm sm:text-base md:text-lg text-center text-gray-50 opacity-80">Ideoxan is empowering the next generation of programmers, makers, and inventors.</h2>
+                    <a href="/signup" className="mx-auto mt-8 bttn bttn-outline bttn-normal bttn-outline-white">Join the Waitlist --&gt;</a>
                 </div>
 
                 {/* Hero image */}
