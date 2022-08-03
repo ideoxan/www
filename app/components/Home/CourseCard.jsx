82607be1cdd4a93f87f60cc552b34cbda76eb349
Skyclo <hello.skyclo@gmail.com>
2022-08-02 21:00:48 -0400

    💫 add hover animation to homepage course cards

--- app/components/Home/CourseCard.jsx
+++ app/components/Home/CourseCard.jsx
@@ -11,16 +11,14 @@ export default function CourseCard({
         throw new Error("Expected title and description to be defined.")
     }
 
-    let classNameString = "flex flex-col px-4 py-3 bg-gray-700 rounded-lg ring-1 ring-gray-500 ring-opacity-50 w-full h-full shadow-lg" + className
-
     return (
-        <div className={classNameString}>
-            <h3 className="font-sans text-gray-50 text-left tracking-tight text-xl font-bold">{title}</h3>
-            {comingSoon ? (
-                <div className="mt-1 bg-primary bg-opacity-50 text-secondary font-sans font-semibold text-2xs rounded-full px-2 py-1 w-max">
+        <div className={"group flex flex-col px-4 py-3 bg-gray-700 rounded-lg ring-0 ring-gray-500 ring-opacity-50 w-full h-full hover:scale-105 hover:translate-y-px " + className}>
+            <h3 className="font-sans text-transparent bg-gradient-white bg-clip-text text-left tracking-tight text-xl font-bold">{title}</h3>
+            {/* {comingSoon ? (
+                <div className="mt-1 bg-secondary bg-opacity-25 text-secondary font-sans font-bold text-2xs rounded-full px-2 py-1 w-max uppercase">
                     Coming Soon
                 </div>
-            ) : null}
+            ) : null} */}
             <p className="paragraph h-full text-sm my-2 text-gray-50 text-left truncate">{description}</p>
             <div className="flex flex-row w-full">
                 <a href={infoURL} className="inline-block text-gray-50 opacity-50 hover:opacity-100 text-sm font-medium tracking-tight">Learn More</a>
