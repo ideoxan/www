a38f8dec8f150b84f0ed61551ae90a7b079d595d
Skyclo <hello.skyclo@gmail.com>
2022-08-03 13:58:37 -0400

    ✨ add 404 page

--- app/root.jsx
+++ app/root.jsx
@@ -7,6 +7,7 @@ import {
     Scripts,
     ScrollRestoration,
     useLoaderData,
+    useCatch
 } from "@remix-run/react"
 
 import styles from 'app/styles/app.css'
@@ -63,6 +64,38 @@ export async function loader({ request }) {
         })
     }
 }
+
+export function CatchBoundary() {
+    const caught = useCatch()
+
+    return (
+        <html lang="en" className="h-full w-full">
+            <head>
+                <Meta />
+                <Links />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+                <link rel="manifest" href="/site.webmanifest"></link>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#6f2bff"></link>
+            </head>
+            <body className="h-full w-full flex flex-col">
+                <div className="m-auto flex flex-col">
+                    <h1 className="mx-auto font-sans font-extrabold tracking-tight text-6xl text-center glow-text-white">{caught.status}</h1>
+                    <h2 className="mx-auto mt-4 font-sans font-semibold tracking-tight text-lg text-center text-gray-50 opacity-80">{caught.statusText}</h2>
+                    <a href="/" className="mx-auto mt-6 bttn bttn-round bttn-normal bttn-gradient bttn-glow">Go Home</a>
+                </div>
+                <ScrollRestoration />
+                <script dangerouslySetInnerHTML={{
+                    __html: `window.env = null`
+                }} />
+                <Scripts />
+                <LiveReload />
+            </body>
+        </html>
+    )
+}
+
 
 export default function App() {
     let loaderData = useLoaderData()
