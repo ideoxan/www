f21429ccf9886fb4d894bb162b8051314be6296f
Skyclo <hello.skyclo@gmail.com>
2022-08-09 01:19:35 -0400

    🎨 format code

--- app/components/Blog/posts.jsx
+++ app/components/Blog/posts.jsx
@@ -1,6 +1,8 @@
 let posts = []
 
-export default posts.sort((a, b) => b.attributes.date - a.attributes.date).reduce((a, e) => {
-    a[e.attributes.slug] ??= e
-    return a
-}, {})
+export default posts
+    .sort((a, b) => b.attributes.date - a.attributes.date)
+    .reduce((a, e) => {
+        a[e.attributes.slug] ??= e
+        return a
+    }, {})
