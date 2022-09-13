0cffb8a75bd7ca2b2548076cb1bba9a14697b017
Skyclo <hello.skyclo@gmail.com>
2022-09-13 16:38:07 -0400

    🚧 update server for cf pages

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,7 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-    serverBuildTarget: "vercel",
+    serverBuildTarget: "cloudflare-pages",
     // When running locally in development mode, we use the built in remix
     // server. This does not understand the vercel lambda module format,
     // so we default back to the standard build output.
