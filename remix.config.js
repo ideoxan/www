a0c3003f23604fe2ea2d1de1141c59bfe57ca4b6
Skyclo <hello.skyclo@gmail.com>
2022-09-14 01:48:40 -0400

    🔧 update wrangler config

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,10 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
     serverBuildTarget: "cloudflare-pages",
-    // When running locally in development mode, we use the built in remix
-    // server. This does not understand the vercel lambda module format,
-    // so we default back to the standard build output.
-    server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+    server: "./server.js",
     ignoredRouteFiles: ["**/.*"],
     appDirectory: "app",
+    devServerBroadcastDelay: 1000,
     // assetsBuildDirectory: "public/build",
     // serverBuildPath: "api/index.js",
     // publicPath: "/build/",
