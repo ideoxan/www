8f3cf4ff748be67aa9e73019819900829d222638
Skyclo <hello.skyclo@gmail.com>
2022-09-15 02:41:32 -0400

    🎨 remove unnecessary vars

--- app/utils/db.client.js
+++ app/utils/db.client.js
@@ -1,16 +1,11 @@
 import { createClient } from "@supabase/supabase-js"
 
-let dbURL = null
-let dbKey = null
-
 if (!window) throw new Error("Window is not defined")
 
 if (!window.env.SUPABASE_URL) throw new Error("Supabase URL is not set")
 if (!window.env.SUPABASE_ANON_KEY) throw new Error("Supabase key is not set")
-dbURL = window.env.SUPABASE_URL
-dbKey = window.env.SUPABASE_ANON_KEY
 
-const supabaseClient = createClient(dbURL, dbKey, {
+const supabaseClient = createClient(window.env.SUPABASE_URL, window.env.SUPABASE_ANON_KEY, {
     autoRefreshToken: false,
     persistSession: false,
 })
