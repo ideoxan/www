9d0e665027de2e0b767976d100992b47564a3cf2
Skyclo <hello.skyclo@gmail.com>
2022-09-15 01:19:47 -0400

    ⚗️ add globals error

--- server.js
+++ server.js
@@ -9,5 +9,8 @@ const handleRequest = createPagesFunctionHandler({
 
 export function onRequest(context) {
     global.env = context.env
+    if (!global.env) {
+        console.error(new Error("Global env is not set"))
+    }
     return handleRequest(context)
 }
