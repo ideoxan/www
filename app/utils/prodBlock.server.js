62435ccdf1d1a4354dfa9f3f67ddd868e0866ad6
Skyclo <hello.skyclo@gmail.com>
2022-10-20 20:53:28 -0400

    🐛 fix prod block context issues

--- app/utils/prodBlock.server.js
+++ app/utils/prodBlock.server.js
@@ -1,5 +1,5 @@
-export default ctx => {
-    if (ctx?.WORKER_ENV === "production")
+export default () => {
+    if (global?.env?.WORKER_ENV === "production")
         throw new Response("WIP", {
             status: 404,
             statusText: "WIP",
