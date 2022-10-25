96475bddc044ccd19fa39cf6b044fba9a8407930
Skyclo <hello.skyclo@gmail.com>
2022-10-24 22:03:53 -0400

    ♻️ flatten rewards section into index

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -3,7 +3,6 @@ import FeaturesItem from "app/components/Home/FeaturesItem"
 import SolutionsCTA from "app/components/Home/SolutionsCTA"
 import CoursesSection from "app/components/Home/CoursesSection"
 import OpenSourceSection from "app/components/Home/OpenSourceSection"
-import RewardsSection from "app/components/Home/RewardsSection"
 import SignupCTA from "app/components/Shared/SignupCTA"
 import Footer from "app/components/Footer"
 import { json, redirect } from "@remix-run/cloudflare"
@@ -153,7 +152,53 @@ export default function Index() {
             </FadeInSection>
             <CoursesSection />
             <OpenSourceSection />
-            <RewardsSection />
+            <FadeInSection>
+                <section id="rewards" className="section flex-col">
+                    {/* Features */}
+                    <div className="section-container flex-col space-x-6 md:flex-row">
+                        {/* Feature Listing Column */}
+                        <div className="my-auto flex flex-col p-6 md:w-1/2">
+                            <FadeInSection direction="right">
+                                <h2 className="section-header">Level up your coding skills</h2>
+                                <div className="mt-4 w-max rounded-full bg-tertiary bg-opacity-25 px-2 py-1 font-sans text-xs font-bold uppercase text-tertiary">
+                                    Coming Soon
+                                </div>
+                                <ul className="mt-6 flex flex-col space-y-8">
+                                    {/* Feature 1 */}
+                                    <FeaturesItem
+                                        icon="Award"
+                                        title="Learn More, Earn More"
+                                        description="Completing a lesson rewards you with Ideoxan Bits, a virtual token system. Depending on the difficulty of a course, more Bits may be rewarded."
+                                    />
+
+                                    {/* Feature 2 */}
+                                    <FeaturesItem
+                                        icon="TrendingUp"
+                                        title="Compete Against Others"
+                                        description="Climb the leaderboards in a friendly community competition. Earn the most points and you might find your place on the leaderboard!"
+                                    />
+
+                                    {/* Feature 3 */}
+                                    <FeaturesItem
+                                        icon="Gift"
+                                        title="Earn Prizes"
+                                        description="In the Ideoxan Marketplace, you can exchange Bits for various prizes. Not only can you earn some cool Ideoxan merch, but you can also take part in limited time offers through our industry partners."
+                                    />
+                                </ul>
+                            </FadeInSection>
+                        </div>
+
+                        {/* Image Column */}
+                        <div className="hidden w-1/2 flex-shrink flex-grow-0 flex-col p-6 md:flex">
+                            <img
+                                src="/images/rewards_sample.png"
+                                className="mx-auto my-auto w-auto flex-shrink"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                </section>
+            </FadeInSection>
             {!session?.user && <SignupCTA />}
             <Footer />
         </main>
