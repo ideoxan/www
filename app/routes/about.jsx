9b96a3b238c2697df34a430a63aedf3ded8dff6e
Skyclo <hello.skyclo@gmail.com>
2022-10-24 21:48:54 -0400

    ♻️ refactor contact section into component

--- app/routes/about.jsx
+++ app/routes/about.jsx
@@ -5,10 +5,10 @@ import { useLoaderData } from "@remix-run/react"
 import { supabaseLocalStrategy } from "app/utils/auth.server.js"
 import { supabaseAdmin } from "app/utils/db.server.js"
 import FadeInSection from "app/components/FadeInSection"
-import { Discord, Github, Twitter } from "@icons-pack/react-simple-icons"
+import { Github, Twitter } from "@icons-pack/react-simple-icons"
 import OpenSourceSection from "app/components/Home/OpenSourceSection"
-import Icon from "app/components/Icon"
 import Header from "app/components/Header"
+import ContactSection from "app/components/Shared/ContactSection"
 
 const teamData = [
     {
@@ -319,142 +319,7 @@ export default function About() {
             <OpenSourceSection />
 
             {/* Contact */}
-            <FadeInSection>
-                <section className="flex w-full flex-col overflow-hidden py-12 md:py-24">
-                    <div className="mx-auto flex w-full max-w-7xl flex-col space-x-6 md:flex-row">
-                        <div className="my-auto flex w-full flex-col p-6">
-                            <h2 className="section-header">Get in touch</h2>
-                            <div className="flex flex-col md:flex-row">
-                                <div className="flex flex-col pr-6 pb-6 md:w-1/3">
-                                    <h3 className="bg-gradient-white mt-6 bg-clip-text pb-2 text-left font-sans text-lg font-bold tracking-tight text-gray-50 text-transparent md:text-xl">
-                                        General Inquires
-                                    </h3>
-                                    <p className="font-style-paragraph mb-4 md:mb-6">
-                                        Have any questions or concerns? Get in touch with us. For
-                                        the fastest response, join our{" "}
-                                        <a
-                                            href="https://discord.gg/jxqKy6r"
-                                            className="underline hover:no-underline">
-                                            Discord
-                                        </a>{" "}
-                                        server and send us a message.
-                                    </p>
-                                    <div className="mt-auto flex flex-row opacity-70">
-                                        <Icon
-                                            name="Phone"
-                                            width={4}
-                                            height={4}
-                                            color="gray-50 my-auto"
-                                            strokeThickness={2}
-                                        />
-                                        <div className="ml-4 flex flex-col">
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                +1 (929) 262 1699
-                                            </p>
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                Mon - Fri, 9am to 5pm EST
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className="mt-4 flex flex-row opacity-70">
-                                        <Icon
-                                            name="Mail"
-                                            width={4}
-                                            height={4}
-                                            color="gray-50 my-auto"
-                                            strokeThickness={2}
-                                        />
-                                        <div className="ml-4 flex flex-col">
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                hello@ideoxan.com
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="flex flex-col pr-6 pb-6 md:w-1/3">
-                                    <h3 className="bg-gradient-white mt-6 bg-clip-text pb-2 text-left font-sans text-lg font-bold tracking-tight text-gray-50 text-transparent md:text-xl">
-                                        Technical Support Line
-                                    </h3>
-                                    <p className="font-style-paragraph mb-4 md:mb-6">
-                                        Are you experiencing a technical problem with the website?
-                                        Is something broken? Need help?
-                                    </p>
-                                    <div className="mt-auto flex flex-row opacity-70">
-                                        <Icon
-                                            name="Phone"
-                                            width={4}
-                                            height={4}
-                                            color="gray-50 my-auto"
-                                            strokeThickness={2}
-                                        />
-                                        <div className="ml-4 flex flex-col">
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                +1 (929) 262 1699
-                                            </p>
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                Mon - Fri, 9am to 5pm EST
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className="mt-4 flex flex-row opacity-70">
-                                        <Icon
-                                            name="Mail"
-                                            width={4}
-                                            height={4}
-                                            color="gray-50 my-auto"
-                                            strokeThickness={2}
-                                        />
-                                        <div className="ml-4 flex flex-col">
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                support@ideoxan.com
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="flex flex-col pr-6 pb-6 md:w-1/3">
-                                    <h3 className="bg-gradient-white mt-6 bg-clip-text pb-2 text-left font-sans text-lg font-bold tracking-tight text-gray-50 text-transparent md:text-xl">
-                                        Socials
-                                    </h3>
-                                    <p className="font-style-paragraph mb-4 md:mb-6">
-                                        Check out our social media profiles to stay up to date with
-                                        the latest news.
-                                    </p>
-                                    <div className="mt-auto flex flex-row opacity-70">
-                                        <Github width={16} className="my-auto text-gray-50" />
-                                        <div className="ml-4 flex flex-col">
-                                            <a
-                                                href="https://github.com/ideoxan"
-                                                className="font-sans text-xs font-normal leading-5 text-gray-50 underline hover:no-underline md:text-sm">
-                                                GitHub
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div className="mt-auto flex flex-row opacity-70">
-                                        <Twitter width={16} className="my-auto text-gray-50" />
-                                        <div className="ml-4 flex flex-col">
-                                            <a
-                                                href="https://twitter.com/ideoxan"
-                                                className="font-sans text-xs font-normal leading-5 text-gray-50 underline hover:no-underline md:text-sm">
-                                                Twitter
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div className="mt-auto flex flex-row opacity-70">
-                                        <Discord width={16} className="my-auto text-gray-50" />
-                                        <div className="ml-4 flex flex-col">
-                                            <a
-                                                href="https://discord.gg/jxqKy6r"
-                                                className="font-sans text-xs font-normal leading-5 text-gray-50 underline hover:no-underline md:text-sm">
-                                                Discord
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </FadeInSection>
+            <ContactSection />
 
             <Footer />
         </>
--- app/routes/solutions.jsx
+++ app/routes/solutions.jsx
@@ -5,7 +5,6 @@ import { useLoaderData } from "@remix-run/react"
 import { supabaseLocalStrategy } from "app/utils/auth.server.js"
 import { supabaseAdmin } from "app/utils/db.server.js"
 import FadeInSection from "app/components/FadeInSection"
-import { Discord, Github, Twitter } from "@icons-pack/react-simple-icons"
 import Icon from "app/components/Icon"
 import ProductsItem from "app/components/Solutions/ProductsItem"
 import PricingTableHeader from "app/components/Solutions/PricingTableHeader"
@@ -14,6 +13,7 @@ import PricingTableRowHeader from "app/components/Solutions/PricingTableRowHeade
 import PricingTableFeatureIncluded from "app/components/Solutions/PricingTableFeatureIncluded"
 import PricingTableFeatureExcluded from "app/components/Solutions/PricingTableFeatureExcluded"
 import PricingTableRow from "app/components/Solutions/PricingTableRow"
+import ContactSection from "app/components/Shared/ContactSection"
 
 export const meta = () => {
     return {
@@ -544,144 +544,7 @@ export default function Solutions() {
             </FadeInSection>
 
             {/* Contact */}
-            <FadeInSection>
-                <section
-                    id="contact"
-                    className="flex w-full flex-col overflow-hidden py-12 md:py-24">
-                    <div className="mx-auto flex w-full max-w-7xl flex-col space-x-6 md:flex-row">
-                        <div className="my-auto flex w-full flex-col p-6">
-                            <h2 className="section-header">Get in touch</h2>
-                            <div className="flex flex-col md:flex-row">
-                                <div className="flex flex-col pr-6 pb-6 md:w-1/3">
-                                    <h3 className="bg-gradient-white mt-6 bg-clip-text pb-2 text-left font-sans text-lg font-bold tracking-tight text-gray-50 text-transparent md:text-xl">
-                                        General Inquires
-                                    </h3>
-                                    <p className="font-style-paragraph mb-4 md:mb-6">
-                                        Have any questions or concerns? Get in touch with us. For
-                                        the fastest response, join our{" "}
-                                        <a
-                                            href="https://discord.gg/jxqKy6r"
-                                            className="underline hover:no-underline">
-                                            Discord
-                                        </a>{" "}
-                                        server and send us a message.
-                                    </p>
-                                    <div className="mt-auto flex flex-row opacity-70">
-                                        <Icon
-                                            name="Phone"
-                                            width={4}
-                                            height={4}
-                                            color="gray-50 my-auto"
-                                            strokeThickness={2}
-                                        />
-                                        <div className="ml-4 flex flex-col">
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                +1 (929) 262 1699
-                                            </p>
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                Mon - Fri, 9am to 5pm EST
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className="mt-4 flex flex-row opacity-70">
-                                        <Icon
-                                            name="Mail"
-                                            width={4}
-                                            height={4}
-                                            color="gray-50 my-auto"
-                                            strokeThickness={2}
-                                        />
-                                        <div className="ml-4 flex flex-col">
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                hello@ideoxan.com
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="flex flex-col pr-6 pb-6 md:w-1/3">
-                                    <h3 className="bg-gradient-white mt-6 bg-clip-text pb-2 text-left font-sans text-lg font-bold tracking-tight text-gray-50 text-transparent md:text-xl">
-                                        Technical Support Line
-                                    </h3>
-                                    <p className="font-style-paragraph mb-4 md:mb-6">
-                                        Are you experiencing a technical problem with the website?
-                                        Is something broken? Need help?
-                                    </p>
-                                    <div className="mt-auto flex flex-row opacity-70">
-                                        <Icon
-                                            name="Phone"
-                                            width={4}
-                                            height={4}
-                                            color="gray-50 my-auto"
-                                            strokeThickness={2}
-                                        />
-                                        <div className="ml-4 flex flex-col">
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                +1 (929) 262 1699
-                                            </p>
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                Mon - Fri, 9am to 5pm EST
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div className="mt-4 flex flex-row opacity-70">
-                                        <Icon
-                                            name="Mail"
-                                            width={4}
-                                            height={4}
-                                            color="gray-50 my-auto"
-                                            strokeThickness={2}
-                                        />
-                                        <div className="ml-4 flex flex-col">
-                                            <p className="font-sans text-xs font-normal leading-5 text-gray-50 md:text-sm">
-                                                support@ideoxan.com
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="flex flex-col pr-6 pb-6 md:w-1/3">
-                                    <h3 className="bg-gradient-white mt-6 bg-clip-text pb-2 text-left font-sans text-lg font-bold tracking-tight text-gray-50 text-transparent md:text-xl">
-                                        Socials
-                                    </h3>
-                                    <p className="font-style-paragraph mb-4 md:mb-6">
-                                        Check out our social media profiles to stay up to date with
-                                        the latest news.
-                                    </p>
-                                    <div className="mt-auto flex flex-row opacity-70">
-                                        <Github width={16} className="my-auto text-gray-50" />
-                                        <div className="ml-4 flex flex-col">
-                                            <a
-                                                href="https://github.com/ideoxan"
-                                                className="font-sans text-xs font-normal leading-5 text-gray-50 underline hover:no-underline md:text-sm">
-                                                GitHub
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div className="mt-auto flex flex-row opacity-70">
-                                        <Twitter width={16} className="my-auto text-gray-50" />
-                                        <div className="ml-4 flex flex-col">
-                                            <a
-                                                href="https://twitter.com/ideoxan"
-                                                className="font-sans text-xs font-normal leading-5 text-gray-50 underline hover:no-underline md:text-sm">
-                                                Twitter
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div className="mt-auto flex flex-row opacity-70">
-                                        <Discord width={16} className="my-auto text-gray-50" />
-                                        <div className="ml-4 flex flex-col">
-                                            <a
-                                                href="https://discord.gg/jxqKy6r"
-                                                className="font-sans text-xs font-normal leading-5 text-gray-50 underline hover:no-underline md:text-sm">
-                                                Discord
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </FadeInSection>
+            <ContactSection />
 
             <Footer />
         </>
