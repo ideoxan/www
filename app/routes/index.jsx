ad794ed1535cfee55530d2e845340b82d94d89d7
Skyclo <hello.skyclo@gmail.com>
2022-10-24 22:00:51 -0400

    ♻️ flatten features section into index

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,5 @@
 import NavigationBar from "app/components/NavigationBar"
-import FeaturesSection from "app/components/Home/FeaturesSection"
+import FeaturesItem from "app/components/Home/FeaturesItem"
 import SolutionsCTA from "app/components/Home/SolutionsCTA"
 import FreeForAllSection from "app/components/Home/FreeForAllSection"
 import CoursesSection from "app/components/Home/CoursesSection"
@@ -71,7 +71,49 @@ export default function Index() {
                     </div>
                 </header>
             </FadeInSection>
-            <FeaturesSection />
+            <FadeInSection>
+                <section id="features" className="section flex-col">
+                    {/* Features */}
+                    <div className="section-container flex-col space-x-6 md:flex-row">
+                        {/* Feature Listing Column */}
+                        <div className="my-auto flex flex-col p-6 md:w-1/2">
+                            <FadeInSection direction="right">
+                                <h2 className="section-header">Empower your future</h2>
+                                <ul className="mt-8 flex flex-col space-y-8">
+                                    {/* Feature 1 */}
+                                    <FeaturesItem
+                                        icon="Box"
+                                        title="Interactive Content"
+                                        description="All courses include interactive quizzes, lessons, and exercises. Each exercise has a certain objective that needs to be completed through the online code editor."
+                                    />
+
+                                    {/* Feature 2 */}
+                                    <FeaturesItem
+                                        icon="GraduationCap"
+                                        title="Professional Grade Courses"
+                                        description="Courses are written by developers with years of programming and computer science experience. Gain technical know-how and job relevant knowledge with ease."
+                                    />
+
+                                    {/* Feature 3 */}
+                                    <FeaturesItem
+                                        icon="Zap"
+                                        title="Take Control"
+                                        description="Learn at your own pace without the pressure from an online instructor. You get to decide your future in CS with the tools of tomorrow."
+                                    />
+                                </ul>
+                            </FadeInSection>
+                        </div>
+                        {/* Image Column */}
+                        <div className="hidden w-1/2 flex-shrink flex-grow-0 flex-col p-6 md:flex">
+                            <img
+                                src="/images/Feat1.png"
+                                className="mx-auto my-auto w-auto flex-shrink"
+                                alt="Two people programming"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </FadeInSection>
             <SolutionsCTA />
             <FreeForAllSection />
             <CoursesSection />
