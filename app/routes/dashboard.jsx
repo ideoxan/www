9d8854ac7539c3bf42d4957d0fdb8bfb388387c3
Skyclo <hello.skyclo@gmail.com>
2023-01-20 01:07:04 -0500

    🛂 improve auth handling

--- app/routes/dashboard.jsx
+++ app/routes/dashboard.jsx
@@ -1,7 +1,6 @@
 import { json, redirect } from "@remix-run/cloudflare"
 import { Outlet, useLoaderData, useMatches } from "@remix-run/react"
-import { supabaseLocalStrategy } from "app/utils/auth.server.js"
-import { supabaseAdmin } from "app/utils/db.server.js"
+import { getAuthData } from "app/utils/auth.server.js"
 import NavigationBar from "app/components/NavigationBar"
 import DashboardSidebarTab from "app/components/Dashboard/DashboardSidebarTab"
 import { useState } from "react"
@@ -12,29 +11,13 @@ export const meta = () => {
     }
 }
 
-export async function loader({ request, context }) {
-    // Check user auth
-    let session = await supabaseLocalStrategy().checkSession(request)
+export async function loader({ request }) {
+    // Load the user data and session
+    let { session, data, error } = await getAuthData(request)
+    // Redirect to login if invalid session
+    if (error == "invalid_session") return redirect("/login", { headers: { "Set-Cookie": "" } })
 
-    // If the user session is bad, redirect to the login page
-    if (session) {
-        let { user } = session
-        if (!user || !user.id) throw redirect("/login")
-
-        // If the user is authenticated, get the user's data from the database
-        let { data: userData, error } = await supabaseAdmin()
-            .from("user_data")
-            .select()
-            .eq("id", user.id)
-
-        if (error) return json({ session: null, userData: null, error })
-
-        if (userData) {
-            return json({ session, userData: userData[0] })
-        }
-    }
-
-    return json({ session: null, userData: null })
+    return json({ session, data, error })
 }
 
 export default function Dashboard() {
